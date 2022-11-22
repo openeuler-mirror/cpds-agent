dfba2018c8053c9d3e6e1f644f570f9062814bea
wei cao <dev16401@linx-info.com>
2022-11-22 17:14:43 +0800

    Add the implementation of obtaining disk usage method

--- src/getsysinfo.c
+++ src/getsysinfo.c
@@ -28,7 +28,34 @@ int get_sysCpuUsage()
 
 int get_sysDiskUsage()
 {
+    FILE *fp;
+    char filesystem[SYS_DISK_NAME_LEN], available[SYS_DISK_NAME_LEN], use[SYS_DISK_NAME_LEN], mounted[SYS_DISK_NAME_LEN], buf[SYS_DISK_BUFF_LEN];
+    double used, blocks, used_rate;
 
+    //读取df命令输出结果返回给指针fp
+    fp = popen("df", "r");
+    if (fp = NULL)
+    {
+        perror("popen error");
+        return -1;
+    }
+
+    //将fp指针指向下一行
+    fgets(buf, SYS_DISK_BUFF_LEN, fp);
+    double dev_total = 0, dev_used = 0;
+
+    //累加每个设备大小之和赋值给dev_total,累加每个设备已用空间之和赋值给dev_used
+    while (6 == fscanf(fp, "%s %lf %lf %s %s %s", filesystem, &blocks, &used, available, use, mounted))
+    {
+        dev_total += blocks;
+        dev_used += used;
+    }
+
+    //计算磁盘使用率
+    used_rate = (dev_used / dev_total) * SYS_100_PERSENT;
+
+    pclose(fp);
+    return used_rate;
 }
 
 float get_sysIoWriteSize()
