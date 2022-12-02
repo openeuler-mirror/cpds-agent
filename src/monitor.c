859258a8896e91eb3187ca9956fea8d73672b7ce
wei cao <dev16401@linx-info.com>
2022-12-02 18:36:12 +0800

    add a mutex to the thread

--- src/monitor.c
+++ src/monitor.c
@@ -8,11 +8,13 @@
 #include "../util/sqlite3/sqlite3.h"
 
 sqlite3 *db;
+pthread_mutex_t mut;
 int main()
 {
     sys_t sys;
     int res;
     pthread_t getinfo_tid, pushinfo_tid;
+    pthread_mutex_init(&mut, NULL);
 
     if(sqlite3_open("test.db", &db) != SQLITE_OK)
     {
