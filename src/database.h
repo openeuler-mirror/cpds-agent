ae80b95004a0bd621fac601d4032986d6b1d48e9
wei cao <dev16401@linx-info.com>
2022-12-02 16:56:13 +0800

    query and print table information

--- src/database.h
+++ src/database.h
@@ -5,9 +5,11 @@
 #define CREATE_SYSINFOTABLE "create table if not exists sysinfotable (id integer primary key, name text, value text);"
 #define INSERT_INTO_SYSINFOTABLE "insert into sysinfotable (id, name, value) values (NULL, '%s', '%4.2f');"
 #define DELETE_IN_TABLE_SYSINFO "delete from sysinfotable where id = %d;"
+#define SELECT_SYSINFOTABLE "select * from sysinfotable;"
 
 int create_sysinfotable(sqlite3 *db);
 int add_record(sqlite3 *db, char *name, float data);
+int display(void *para,int ncol,char *col_val[],char ** col_name);
 
 #endif
 
