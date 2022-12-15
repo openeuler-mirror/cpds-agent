a371f66b24cfe804ec05ebc0213b8e2f8968caf8
wei cao <dev16401@linx-info.com>
2022-12-16 00:57:41 +0800

    adjust the code spacing in the monitor.h file

--- src/monitor.h
+++ src/monitor.h
@@ -4,18 +4,18 @@
 #define RESULT_FAILED -1
 #define RESULT_SUCCESS 0
 
-#define SYS_DISK_BUFF_LEN 256
-#define SYS_DISK_NAME_LEN 80
-#define SYS_100_PERSENT 100
-#define MAXBUFSIZE 1024
-#define WAIT_SECOND 3 
+#define SYS_DISK_BUFF_LEN   256
+#define SYS_DISK_NAME_LEN   80
+#define SYS_100_PERSENT     100
+#define MAXBUFSIZE          1024
+#define WAIT_SECOND         3 
 
-#define FIELD_CPU_USAGE "CpuUsage"
-#define FIELD_DISK_USAGE  "DiskUsage"
-#define FIELD_IO_WRITESIZE  "IoWriteSize"
+#define FIELD_CPU_USAGE      "CpuUsage"
+#define FIELD_DISK_USAGE     "DiskUsage"
+#define FIELD_IO_WRITESIZE   "IoWriteSize"
 
-#define CONFIG_PATH "/etc/cpds/cpds-agent/cpds_log.conf"
-#define CPDSCLASS_NAME "cpds-agent"
+#define CONFIG_PATH          "/etc/cpds/cpds-agent/cpds_log.conf"
+#define CPDSCLASS_NAME       "cpds-agent"
 typedef struct systeminfo
 {
     double CpuUsage;
