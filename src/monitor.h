866595cb09ded7b689ff19bf3204724ff169aa06
wei cao <dev16401@linx-info.com>
2023-02-06 15:45:40 +0800

    adjust the macro definition code style for the maximum variable in the monitor.h file

--- src/monitor.h
+++ src/monitor.h
@@ -7,7 +7,7 @@
 #define  SYS_DISK_BUFF_LEN   256
 #define  SYS_DISK_NAME_LEN   80
 #define  SYS_100_PERSENT     100
-#define MAXBUFSIZE          1024
+#define  MAXBUFSIZE          1024
 #define WAIT_SECOND         3 
 
 #define FIELD_CPU_USAGE      "cpuusage"
