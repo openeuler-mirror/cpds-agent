fbcfdfcb8c788adfb40cc9df6239bfde5b391eef
wei cao <dev16401@linx-info.com>
2023-02-06 21:05:50 +0800

    Modify the spacing of the pclose function call code in the method that gets the io usage

--- src/getsysinfo.c
+++ src/getsysinfo.c
@@ -221,7 +221,6 @@ float get_sysio_wbs()
     fgets(buffer, sizeof(buffer), fp);
     sscanf(buffer, "%s %f %f %f %f %f %f %f %f %f %f %f %f %f ", a, &arr[0], &arr[1], &arr[2], &arr[3], &arr[4], &arr[5], &arr[6], &arr[7], &arr[8], &arr[9], &arr[10], &arr[11], &arr[12]);
     CPDS_ZLOG_DEBUG(" wareq-sz : %f", arr[12]);
-
     pclose(fp);
     return arr[12];
 }
