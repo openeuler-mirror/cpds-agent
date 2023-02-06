7f441a0fc0c43fa4d73de745f4d55bfb97b43ed5
wei cao <dev16401@linx-info.com>
2023-02-06 16:54:05 +0800

    change the style of the method code for obtaining disk usage in the monitor.h file

--- src/monitor.h
+++ src/monitor.h
@@ -43,7 +43,7 @@ typedef struct cpu_info {
 void  *get_sysinfo(void *arg);
 void  *push_sysinfo(void *arg);
 float  get_syscpu_usage();
-double get_sysdisk_usage();
+double  get_sysdisk_usage();
 float get_sysio_wbs();
 
 #endif
